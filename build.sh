aa057c851aaa6fb8a9dd868eeee342ee0129ef6e
kestrel84 <maximilian.ashton@gmail.com>
2026-08-04 18:48:20 +0100

    build script update 2

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 main() {
+    echo "Running custom build script"
+
     ZOLA_VERSION=0.22.1
 
     curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
     tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
 
     ./zola -c zola-site/zola.toml build
+    
+    echo "Custom build script complete"
 }
 
 set -euo pipefail
