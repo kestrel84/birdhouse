befa054dd380aedd7a7fd181828147c441236e8b
kestrel84 <maximilian.ashton@gmail.com>
2026-08-04 19:00:09 +0100

    build fixes

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 main() {
     echo "Running custom build script"
+    cd zola-site/
 
     ZOLA_VERSION=0.22.1
 
     curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
     tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
 
-    ./zola -c zola-site/zola.toml build
+    ./zola build
     
     echo "Custom build script complete"
 }
