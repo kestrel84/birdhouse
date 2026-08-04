a77f48e35cf4b349ea698d260ed26f97d04df85c
kestrel84 <maximilian.ashton@gmail.com>
2026-08-04 18:29:53 +0100

    wrangler.toml and build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+main() {
+    ZOLA_VERSION=0.22.1
+
+    curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+    tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+
+    ./zola -c zola-site/zola.toml build
+}
+
+set -euo pipefail
